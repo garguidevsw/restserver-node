bd8a7370262210325acd33bddd41a6741f1f5e84
Julio Cesar Garcia Guillen <garguidevsw@gmail.com>
2022-04-03 14:08:41 -0500

    Se añadio Google Identify

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,8 @@ const bcryptjs = require("bcryptjs");
 
 const User = require('../models/user');
 const { generateJWT } = require("../helpers/generate-jwt");
+const { googleVerify } = require("../helpers/google-verify");
+
 
 const login = async(req, res = response) => {
 
@@ -51,6 +53,49 @@ const login = async(req, res = response) => {
     
 }
 
+const googleSignIn = async( req, res = response) => {
+    const { id_token } = req.body;
+
+    try {
+        const { name, image, email } = await googleVerify(id_token);
+        
+        let user = await User.findOne({email});
+        if(!user){
+            const data = {
+                name,
+                email,
+                password: ':P',
+                image,
+                google: true
+            }
+
+            user = new User(data);
+            await user.save();
+        }
+
+        if(!user.status){
+            return res.status(401).json({
+                ok: false,
+                msg: 'Hable con el administrador, usuario boqueado.'
+            });
+        }
+
+        // Generar JWT
+        const token = await generateJWT( user.id );
+
+        res.json({
+            user,
+            token
+        });
+    } catch (error) {
+        res.status(400).json({
+            ok: false,
+            msg: 'El token no se pudo verificar.'
+        });
+    }
+}
+
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
-const { login } = require('../controllers/auth');
+const { login, googleSignIn } = require('../controllers/auth');
 const { validator } = require('../middlewares/validator');
 
 const router = Router();
@@ -12,5 +12,10 @@ router.post('/login', [
     check('password', 'La contraseña es obligatoria').not().isEmpty(),
     validator
 ], login);
+
+router.post('/google', [
+    check('id_token', 'El id token es obligatorio').not().isEmpty(),
+    validator
+], googleSignIn);
 
 module.exports = router;
